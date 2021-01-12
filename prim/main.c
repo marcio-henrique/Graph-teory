ba94eeb8319521519e86f91f09063173c6f3700a
Márcio Henrique <ikke.guerreiro@gmail.com>
2021-01-11 22:45:37 -0300

    main com exemplo do professor

--- prim/main.c
+++ prim/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include "graph.h"
+#include "prim.h"
 int main() {
-    printf("Hello, World!\n");
+    Graph *graph = create_graph(6);
+    graph = add_weighted_simple_edge(graph, 0, 1, 5);
+    graph = add_weighted_simple_edge(graph, 0, 2, 6);
+    graph = add_weighted_simple_edge(graph, 0, 3, 4);
+    graph = add_weighted_simple_edge(graph, 1, 2, 1);
+    graph = add_weighted_simple_edge(graph, 1, 3, 2);
+    graph = add_weighted_simple_edge(graph, 2, 3, 2);
+    graph = add_weighted_simple_edge(graph, 2, 4, 7);
+    graph = add_weighted_simple_edge(graph, 2, 5, 3);
+    graph = add_weighted_simple_edge(graph, 3, 5, 4);
+    graph = add_weighted_simple_edge(graph, 4, 5, 4);
+
+    graph = prim_algorithm(graph, 0);
+    print_MST(graph->vertex, graph->vertex_quantity);
+
     return 0;
 }
