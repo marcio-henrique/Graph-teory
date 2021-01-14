4468eecc74b1cecccc89fbc973cbae2c18436ca3
Márcio Henrique <ikke.guerreiro@gmail.com>
2021-01-14 17:12:18 -0300

    Revert "Correção de bug na função add_weighted_simple_edge do grafo"
    This reverts commit 18448c80a0546688f7a61021aee47cd196e4ef77.

--- floyd-warshall/floyd_warshall.c
+++ floyd-warshall/floyd_warshall.c
@@ -4,56 +4,58 @@
 
 #include "floyd_warshall.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <limits.h>
 
 void floyd_warshall_algorithm (Graph *graph) {
-    lli **minimal_distancies;
+    lli minimal_distancies[graph->vertex_quantity][graph->vertex_quantity];
 
-    minimal_distancies = initialize_distancies(graph->adjacency_matrix, graph->vertex_quantity);
-
-//    for (lli i = 0; i < graph->vertex_quantity; i++) {
-//        for (lli j = 0; j < graph->vertex_quantity; j++) {
-//            minimal_distancies[i][j] = graph->adjacency_matrix[i][j];
-//        }
-//    }
+//    initialize_distancies(graph->adjacency_matrix, (lli **) minimal_distancies, graph->vertex_quantity);
+    for (lli i = 0; i < graph->vertex_quantity; i++) {
+        for (lli j = 0; j < graph->vertex_quantity; j++) {
+            minimal_distancies[i][j] = graph->adjacency_matrix[i][j];
+        }
+    }
 
     for (lli k = 0; k < graph->vertex_quantity; k++) {
         for (lli i = 0; i < graph->vertex_quantity; i++) {
             for (lli j = 0; j < graph->vertex_quantity; j++) {
-                if (minimal_distancies[i][k] != LLONG_MAX && minimal_distancies[k][j] != LLONG_MAX) {
-                    minimal_distancies[i][j] = min(minimal_distancies[i][j], minimal_distancies[i][k] + minimal_distancies[k][j]);
-                }
+                minimal_distancies[i][j] = min(minimal_distancies[i][j], minimal_distancies[i][k] + minimal_distancies[k][j]);
             }
         }
     }
-    printMatrix(minimal_distancies, graph->vertex_quantity);
+//    printMatrix((lli **) minimal_distancies, graph->vertex_quantity);
 
+    for (lli i = 0; i < graph->vertex_quantity; i++) {
+        for (lli j = 0; j < graph->vertex_quantity; j++) {
+            if (minimal_distancies[i][j] == LLONG_MAX) {
+                printf("INF.\t");
+            } else {
+                printf("%lld\t", minimal_distancies[i][j]);
+            }
+        }
+        printf("\n");
+    }
 }
 
 lli min(lli a, lli b) {
     return a < b ? a : b;
 }
 
-lli **initialize_distancies(lli **adjacency_matrix, lli size) {
-    lli **minimal_distancies = (lli **) malloc(size * sizeof (lli *));
+void initialize_distancies(lli **adjacency_matrix, lli **minimal_distancies, lli size) {
     for (lli i = 0; i < size; i++) {
-        minimal_distancies[i] = (lli *) malloc(size * (sizeof (lli)));
         for (lli j = 0; j < size; j++) {
             minimal_distancies[i][j] = adjacency_matrix[i][j];
         }
     }
-
-    return minimal_distancies;
 };
 
-void printMatrix(lli **minimal_distancies, lli size) {
+void printMatrix(lli **matrix, lli size) {
     for (lli i = 0; i < size; i++) {
         for (lli j = 0; j < size; j++) {
-            if (minimal_distancies[i][j] == LLONG_MAX) {
+            if (matrix[i][j] == LLONG_MAX) {
                 printf("INF.\t");
             } else {
-                printf("%lld\t", minimal_distancies[i][j]);
+                printf("%lld\t", matrix[i][j]);
             }
         }
         printf("\n");
