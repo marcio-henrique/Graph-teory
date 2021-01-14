8f1912390827dcce6e541f3a59ea06cba953e57c
Márcio Henrique <ikke.guerreiro@gmail.com>
2021-01-14 16:09:20 -0300

    Grafo com matriz de adjacência

---
+++ floyd-warshall/graph.c
@@ -0,0 +1,42 @@
+//
+// Created by ikkeg on 07/01/2021.
+//
+#include <stdlib.h>
+#include <limits.h>
+#include "graph.h"
+
+Graph *create_graph(lli vertex_quantity) {
+    Graph *graph = (Graph *) malloc(sizeof(Graph));
+    graph->vertex_quantity = vertex_quantity;
+
+    graph->adjacency_matrix = create_adjacency_matrix(vertex_quantity);
+
+    return graph;
+}
+
+lli **create_adjacency_matrix(lli vertex_quantity) {
+    lli **new_adjacency_matrix = (lli **) malloc(vertex_quantity * sizeof (lli *));
+    for(lli i = 0; i < vertex_quantity; i++) {
+        new_adjacency_matrix[i] = (lli *) malloc(vertex_quantity * (sizeof (lli)));
+        for(lli j = 0; j < vertex_quantity; j++) {
+            if (i == j) {
+                new_adjacency_matrix[i][j] = 0;
+            } else {
+                new_adjacency_matrix[i][j] = LLONG_MAX;
+            }
+        }
+    }
+    return new_adjacency_matrix;
+}
+
+Graph *add_weighted_simple_edge (Graph *graph, lli vertex1, lli vertex2, lli weight) {
+    graph->adjacency_matrix[vertex1][vertex2] = weight;
+    graph->adjacency_matrix[vertex2][vertex1] = weight;
+
+}
+
+Graph *add_one_directed_edge (Graph *graph, lli vertex1, lli vertex2, lli weight) {
+    graph->adjacency_matrix[vertex1][vertex2] = weight;
+
+    return graph;
+}
---
+++ floyd-warshall/graph.h
@@ -0,0 +1,26 @@
+//
+// Created by ikkeg on 07/01/2021.
+//
+
+#ifndef FLOYD_WARSHALL_GRAPH_H
+#define FLOYD_WARSHALL_GRAPH_H
+
+typedef long long int lli;
+
+typedef struct Graph Graph;
+
+struct Graph
+{
+    lli **adjacency_matrix;
+    lli vertex_quantity;
+};
+
+Graph *create_graph(lli vertex_quantity);
+
+lli **create_adjacency_matrix(lli vertex_quantity);
+
+Graph *add_weighted_simple_edge (Graph *graph, lli vertex1, lli vertex2, lli weight);
+
+Graph *add_one_directed_edge (Graph *graph, lli vertex1, lli vertex2, lli weight);
+
+#endif //FLOYD_WARSHALL_GRAPH_H
